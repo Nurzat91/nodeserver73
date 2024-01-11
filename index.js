8dbd2be963417d55100dbbce17c0f180c05169a1
Nurzat Taalaibekova <tnurzat91@gmail.com>
2024-01-11 10:28:26 +0300

    completed task 1

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const port = 8000;
+
+app.get('/:name', (req, res) => {
+  res.send(req.params.name);
+});
+
+
+app.listen(port, () => {
+  console.log(port);
+});
