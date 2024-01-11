76dcd1bd6d5809b42e50ace9d3680a66809f600f
Nurzat Taalaibekova <tnurzat91@gmail.com>
2024-01-11 12:35:25 +0300

    text encryption and decryption

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const express = require('express');
 const app = express();
 
 const port = 8000;
+const Vigenere = require('caesar-salad').Vigenere;
 
 app.get('/:name', (req, res) => {
   res.send(req.params.name);
 });
 
+app.get('/encode/:text', (req, res) => {
+  const result = Vigenere.Cipher('password').crypt(req.params.text);
+  res.send(result);
+});
+
+app.get('/decode/:text', (req, res) => {
+  const result = Vigenere.Decipher('password').crypt(req.params.text);
+  res.send(result);
+});
+
 
 app.listen(port, () => {
-  console.log(port);
+  console.log('Server is running on', port);
 });
